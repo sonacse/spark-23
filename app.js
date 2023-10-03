21e97d570da0ab039cb1332aba53f93ba460e1f6
sonacse <146024960+sonacse@users.noreply.github.com>
2023-10-03 23:07:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,141 +1,202 @@
-let data={};
+const firebaseConfig = {
+    apiKey: "AIzaSyDBQbY94T_SdMJl4bs4iWz6QKc7YnBZd6o",
+    authDomain: "spark-23.firebaseapp.com",
+    databaseURL: "https://spark-23-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "spark-23",
+    storageBucket: "spark-23.appspot.com",
+    messagingSenderId: "899844768435",
+    appId: "1:899844768435:web:29f0e4881816cf1ac6e6c6",
+    measurementId: "G-DZ0MT180V7"
+};
 
-document.getElementById('nextbtn').addEventListener('click',event=>{
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+var database = firebase.database();
+var storage = firebase.storage();
+
+var registerdata = database.ref("data");
+var count = database.ref("count");
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function eventshow()
+{
+    const option = document.querySelectorAll('.op');
+    option.forEach((element) => {
+        const inputElement = element.querySelector('.gen');
+        const name = inputElement.getAttribute('name');        
+        database.ref('count/' + name).once('value').then((snapshot) => {
+            const data = snapshot.val();
+            let length = Object.keys(data).length;
+            if (length >= 90) {
+                console.log(name)
+                element.style.display = 'none';
+            }
+        })
+    });
+}
+// //////////////////////////////////////////////////////////////////////////////////////////////
+
+let data = {};
+
+document.getElementById('nextbtn').addEventListener('click', async (event) => {
     event.preventDefault();
     const formControls = document.querySelectorAll('.form1');
-    let check=0;
-    formControls.forEach(element => {
-        if(element.value=="")
-        {
+    let check = 0;
+
+    // Create an array to hold promises
+    const promises = [];
+
+    for (const element of formControls) {
+        if (element.value === "") {
             swal({
-                icon:'error',
-                title:'Please Enter all the Details'
-            })
-            check=1;
+                icon: 'error',
+                title: 'Please Enter all the Details'
+            });
+            check = 1;
+            break;
+        } else {
+            if (element.name === "regno") {
+                const snapshot = await registerdata.child(element.value).get();
+                if (snapshot.exists()) {
+                    swal("Error", "The register no is already taken.", "error");
+                    check = 1;
+                } else {
+                    data[element.name] = element.value;
+                }
+            } else {
+                data[element.name] = element.value;
+            }
         }
-        else{
-            data[element.name] = element.value;
-        }
-    });
-    if(check==0)
-    {
-        
-        document.getElementById('dataform1').style.display='none';
-        
-        if(data.dep.includes("cse"))
-        {
-            document.getElementById('dataform3').style.display='block';
-        }
-        else{
-            document.getElementById('dataform2').style.display='block';
-        }
-       
-        formControls.forEach(element => {
-            element.value=""
-        })
     }
-})
 
+    // Wait for all promises to resolve
+    await Promise.all(promises);
 
+    if (check === 0) {
+        document.getElementById('dataform1').style.display = 'none';
+
+        if (data.dep.includes("cse")) {
+            eventshow()
+            document.getElementById('dataform3').style.display = 'block';
+        } else {
+            document.getElementById('dataform2').style.display = 'block';
+        }
+
+        formControls.forEach((element) => {
+            element.value = "";
+        });
+
+    }
+});
 
 document.getElementById('eventdetails').addEventListener('click', event => {
-    let check2=0;
-    let total=0;
     event.preventDefault();
-    const tevent = document.getElementById("tevent");
-    const ntevent = document.getElementById("ntevent");
-    const cevent = document.getElementById("cevent");
 
-    // Check if at least one checkbox is checked
-    if (!tevent.checked && !ntevent.checked && !cevent.checked) {
+    let otherreg=document.getElementById('otherreg')
+
+     if (!otherreg.checked) {
         swal({
             icon: 'error',
-            title: 'Please select at least one event'
+            title: 'Please ( ✓ ) The Check Box That Present In The End Of The Page, To Register Your response.'
         });
-        check2=1;
-       
+      
     }
 
-    // If any checkbox is checked, proceed to store the data
-    if (tevent.checked) {
-        data[tevent.name] = 'yes';
-        total += 150;
-    } else {
-        data[tevent.name] = 'no';
+    if (otherreg.checked) {
+        data['paid'] = "No";
+        otherreg.checked = false;
+        loaddata("other");
     }
-    if (ntevent.checked) {
-        data[ntevent.name] = 'yes';
-        total += 150;
-    } else {
-        data[ntevent.name] = 'no';
+
+    
+
+});
+
+document.getElementById('csestudentevent').addEventListener('click', event => {
+    event.preventDefault();
+   let getcsestudentevent1=document.querySelectorAll('.getcsestudentevent1')
+   let getcsestudentevent2=document.querySelectorAll('.getcsestudentevent2')
+
+   let techname=[];
+   let nontechname=[];
+
+   getcsestudentevent1.forEach(element=>{
+        if(element.checked)
+        {
+            techname.push(element.name)
+        }
+   })
+
+   getcsestudentevent2.forEach(element=>{
+    if(element.checked)
+    {
+        nontechname.push(element.name)
     }
-    if (cevent.checked) {
-        data[cevent.name] = 'yes';
-        total += 150;
-    } else {
-        data[cevent.name] = 'no';
+    })
+
+    if(techname.length==2 && nontechname.length==2 )
+    {
+        for(let i=0;i<2;i++)
+        {
+            data['Technical'+(i+1)] = techname[i];
+            data['NonTechnical'+(i+1)] = nontechname[i];
+        }
+        getcsestudentevent2.forEach(element=>{
+            element.checked = false;
+       })
+       getcsestudentevent1.forEach(element=>{
+        if(element.checked)
+        {
+            element.checked = false;
+        }
+   })
+        loaddata("cse");
     }
-    data['total'] = total;
-    if(check2==0)
-    {console.log(data)
+    else
+    {
+        console.log(techname.length);
+        console.log(nontechname.length)
         swal({
-            icon:'success',
-            title:'Your Event is Registered Successfully',
-            text:'You want to pay the Total amount RS: '+total+' /-, During on the days of the event.'
+            icon: 'error',
+            title: 'Exactly You Want To Select 2 - Technical Event & 2 - Non-Technical Event only',
         })
-        tevent.checked = false;
-        ntevent.checked = false;
-        cevent.checked = false;
-        document.getElementById('dataform2').style.display='none';
-        document.getElementById('dataform1').style.display='block';
-
     }
-
 
 });
 
 
-document.getElementById('csestudentevent').addEventListener('click',event=>{
-    let check3=0;
-    event.preventDefault();
-    const getdata=document.querySelectorAll('.form2')
-    getdata.forEach(element => {
-        if(element.value=="")
-        {
-            swal({
-                icon:'error',
-                title:'You Must Select Two Technical and Two Non-Technical Event'
-            })
-            check3=1;
+function loaddata(type) {
+    console.log(data)
+    if (type == "cse") {
+        let ename = [data.NonTechnical1, data.NonTechnical2, data.Technical1, data.Technical2];
+        for (let i = 0; i < 4; i++) {
+            count.child(ename[i]).child(data.regno).set({
+                regno: data.regno
+            });
         }
-        else{
-            data[element.name] = element.value;
+    }
+    registerdata.child(data.regno).set(data, function (error) {
+        if (error) {
+            console.error("Data could not be saved.", error);
+        } else {
+            swal({
+                icon: 'success',
+                title: 'Your Event is Registered Successfully'
+            });
+            document.getElementById('dataform2').style.display = 'none';
+            document.getElementById('dataform3').style.display = 'none';
+            document.getElementById('dataform1').style.display = 'block';
         }
     });
-    const cseclubevent=document.getElementById('cseclubevent')
-    if(cseclubevent.checked)
-    {
-        data[cseclubevent.name] = 'yes';
-    }
-    else{
-        data[cseclubevent.name] = 'no';
-    }
-    console.log(data)
-    if(check3==0)
-    {
-        swal({
-            icon:'success',
-            title:'Your Event is Registered Successfully'
-        })
-        getdata.forEach(element => {
-            element.value=""
-        })
-        document.getElementById('dataform3').style.display='none';
-        document.getElementById('dataform1').style.display='block';
-
-    }
-
-})
+    data = {}
+}
 
 
 
+
+// console.log(Object.keys(arr).length)
